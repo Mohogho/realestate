518f24177e4a12167a0f59bedcf08f9ba215184d
mohammad hossein gholamhosseini <mhght2008@gmail.com>
2025-06-03 21:09:10 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    // centeredSlides: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
-
 document.addEventListener("DOMContentLoaded", function () {
     const labels = document.querySelectorAll(".leftPanel label");
 
